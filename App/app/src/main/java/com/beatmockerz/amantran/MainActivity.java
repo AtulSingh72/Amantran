44fa348753332a0d57749c495e405ec2b4b6dd7f
AtulSingh72 <atul.as72@gmail.com>
2020-12-26 13:17:37 +0530

    Basic UI and Volley Setup Done

--- App/app/src/main/java/com/beatmockerz/amantran/MainActivity.java
+++ App/app/src/main/java/com/beatmockerz/amantran/MainActivity.java
@@ -2,17 +2,33 @@ package com.beatmockerz.amantran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText name;
     Button submit;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +36,45 @@ public class MainActivity extends AppCompatActivity {
 
         name = (EditText) findViewById(R.id.name);
         submit = (Button) findViewById(R.id.submit);
+        sharedPreferences = getSharedPreferences("Amantran", Context.MODE_PRIVATE);
 
     }
 
-    public void submitName(View view) {
+    public void submitName(View view) throws JSONException {
         String person_name = name.getText().toString();
-        JSONObject data = new JSONObject();
-        data.put("name", person_name);
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        String url = "http://192.168.1.5:8000/name";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    Log.i("ID", obj.getString("id"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.i("POST Response", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", person_name);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-type", "application/x-www-form-urlencoded");
+                return headers;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 }
