7a0c49c6d4cdb1d4d378b00d7b6b576062c67a7e
AtulSingh72 <atul.as72@gmail.com>
2020-12-26 19:28:53 +0530

    Add new member implemented

--- App/app/src/main/java/com/beatmockerz/amantran/MainActivity3.java
+++ App/app/src/main/java/com/beatmockerz/amantran/MainActivity3.java
@@ -1,5 +1,6 @@
 package com.beatmockerz.amantran;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.android.volley.Request;
@@ -28,6 +29,7 @@ public class MainActivity3 extends AppCompatActivity {
 
     TextView text;
     ListView list;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,7 @@ public class MainActivity3 extends AppCompatActivity {
 
         text = (TextView) findViewById(R.id.text);
         list = (ListView) findViewById(R.id.list);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         if(getIntent().getStringExtra("type").equals("friends")) {
             text.setText("Friends List");
@@ -46,6 +49,17 @@ public class MainActivity3 extends AppCompatActivity {
                 @Override
                 public void onResponse(String response) {
                     try {
+                        fab.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                                intent.putExtra("type", "friends");
+                                intent.putExtra("edit", "false");
+                                intent.putExtra("id", user_id);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
                         JSONObject obj = new JSONObject(response);
                         JSONArray jsonArray = obj.getJSONArray("friends");
                         String guest_list[] = new String[jsonArray.length()];
@@ -78,6 +92,17 @@ public class MainActivity3 extends AppCompatActivity {
                 @Override
                 public void onResponse(String response) {
                     try {
+                        fab.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
+                                intent.putExtra("edit", "false");
+                                intent.putExtra("type", "family");
+                                intent.putExtra("id", user_id);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
                         JSONObject obj = new JSONObject(response);
                         JSONArray jsonArray = obj.getJSONArray("family");
                         String guest_list[] = new String[jsonArray.length()];
--- App/app/src/main/java/com/beatmockerz/amantran/MainActivity4.java
+++ App/app/src/main/java/com/beatmockerz/amantran/MainActivity4.java
@@ -2,13 +2,96 @@ package com.beatmockerz.amantran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    LinearLayout proceed;
+    EditText guestname;
+    String type;
+    String user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        proceed = (LinearLayout) findViewById(R.id.proceed);
+        guestname = (EditText) findViewById(R.id.guestname);
+        if(getIntent().getStringExtra("edit").equals("false")) {
+            // new guest entry
+            user_id = getIntent().getStringExtra("id");
+            type = getIntent().getStringExtra("type");
+
+            Button save = new Button(MainActivity4.this);
+            save.setText("Save");
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = 10;
+            params.rightMargin = 10;
+            params.gravity = Gravity.CENTER;
+            save.setLayoutParams(params);
+            save.setOnClickListener(save_click);
+            proceed.addView(save);
+        }
+        else {
+            // edit or delete guest entry
+        }
     }
+
+    View.OnClickListener save_click = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = guestname.getText().toString();
+            String url = "http://192.168.1.5:8000/" + type + "/" + user_id;
+            RequestQueue requestQueue = Volley.newRequestQueue(MainActivity4.this);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Intent intent = new Intent(MainActivity4.this, MainActivity3.class);
+                    intent.putExtra("type", type);
+                    intent.putExtra("id", user_id);
+                    startActivity(intent);
+                    finish();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("POST Response", error.toString());
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("name", name);
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Content-type", "application/x-www-form-urlencoded");
+                    return headers;
+                }
+            };
+            requestQueue.add(stringRequest);
+        }
+    };
 }
